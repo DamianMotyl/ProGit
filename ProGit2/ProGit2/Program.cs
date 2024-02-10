d997fd07b11c6ca867849cd2bba285660ab7ccfb
DamianMotyl <damian.motyl@outlook.com>
2024-02-10 21:51:04 +0100

    WriteLine z wiersza poleceń

--- ProGit2/ProGit2/Program.cs
+++ ProGit2/ProGit2/Program.cs
@@ -3,7 +3,10 @@
 public class Program
 { public void PrintHello()
     { Console.WriteLine("Witaj, świecie");
-        Console.WriteLine("Własnie dodałem tę linijkę w Visual Studio");
+      Console.WriteLine("Własnie dodałem tę linijkę w Visual Studio");
+        Console.WriteLine("Właśnie dodałem tę linijkę w repozytorium wiersza poleceń");
+
+
 
     }
 }
